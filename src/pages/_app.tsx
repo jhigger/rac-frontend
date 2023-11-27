ddc908376a45d7768d5b9faafd81eed55ba5a7d2
jhigger <njohnjhigger@gmail.com>
2023-11-27 22:28:59 +0800

    add design system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { useEffect } from "react";
+import tailmater from "~/js/tailmater";
+import { Roboto } from "next/font/google";
 
 import "~/styles/globals.css";
 
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    tailmater();
+  }, []);
+
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default MyApp;

77510011142e75a793f0dd30a873c74529c1b9c5
jhigger <njohnjhigger@gmail.com>
2023-11-29 20:26:11 +0800

    add tabs to context provider

--- src/contexts/TabsContext.tsx
+++ src/contexts/TabsContext.tsx
@@ -12,8 +12,11 @@ import RequestsPanel from "~/components/Shop/RequestsPanel";
 
 export type TabsContextType = {
   activeTab: string;
+  requestOrderClicked: boolean;
+  tabs: AppBarTabType[];
   tabsRef: MutableRefObject<HTMLButtonElement[]>;
   handleTabChange: (tab: string) => void;
+  handleRequestOrder: (value: boolean) => void;
 };
 
 export const TabsContext = createContext<TabsContextType>(
@@ -36,16 +39,25 @@ export const tabs: AppBarTabType[] = [
 
 const TabsContextProvider = ({ children }: { children: ReactNode }) => {
   const [activeTab, setActiveTab] = useState(tabs[0]?.id ?? "orders");
+  const [requestOrderClicked, setRequestOrderClicked] = useState(false);
   const tabsRef = useRef<HTMLButtonElement[]>([]);
 
   const handleTabChange = (tab: string) => {
     setActiveTab(tab);
+    setRequestOrderClicked(false);
+  };
+
+  const handleRequestOrder = (value: boolean) => {
+    setRequestOrderClicked(value);
   };
 
   const value: TabsContextType = {
     activeTab,
+    requestOrderClicked,
+    tabs,
     tabsRef,
     handleTabChange,
+    handleRequestOrder,
   };
 
   return <TabsContext.Provider value={value}>{children}</TabsContext.Provider>;

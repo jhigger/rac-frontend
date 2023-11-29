262967740c2db5b574c3ad17290adceab2611c2e
jhigger <njohnjhigger@gmail.com>
2023-11-29 23:59:09 +0800

    edit

--- src/components/Forms/PasswordInput.tsx
+++ src/components/Forms/PasswordInput.tsx
@@ -1,10 +1,10 @@
-import { useState, type ChangeEvent } from "react";
+import { useState, type ChangeEventHandler } from "react";
 
 type PasswordInputProps = {
   id: string;
   label: string;
   value?: string;
-  onChange?: (update: ChangeEvent<HTMLInputElement>) => void;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
 };
 
 const PasswordInput = ({ id, label, value, onChange }: PasswordInputProps) => {
@@ -29,7 +29,7 @@ const PasswordInput = ({ id, label, value, onChange }: PasswordInputProps) => {
 
         <input
           type={show ? "text" : "password"}
-          aria-label="Password"
+          aria-label={label}
           name={id}
           id={id}
           className="peer relative block h-14 w-full overflow-x-auto rounded-[20px] border border-gray-500 bg-neutral-10 py-2 pl-4 pr-14 leading-5 focus:border-2 focus:border-primary-600 focus:outline-none focus:ring-0"
@@ -38,10 +38,7 @@ const PasswordInput = ({ id, label, value, onChange }: PasswordInputProps) => {
           onChange={onChange}
         />
 
-        <label
-          htmlFor={id}
-          className="absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform bg-neutral-10 px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:bg-neutral-10 peer-focus:text-primary-600"
-        >
+        <label className="absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform bg-neutral-10 px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:bg-neutral-10 peer-focus:text-primary-600">
           {label}
         </label>
       </div>
--- src/components/Forms/TextInput.tsx
+++ src/components/Forms/TextInput.tsx
@@ -1,11 +1,11 @@
-import { type ChangeEvent, type HTMLInputTypeAttribute } from "react";
+import { type ChangeEventHandler, type HTMLInputTypeAttribute } from "react";
 
 type TextInputProps = {
   id: string;
   label: string;
-  type?: HTMLInputTypeAttribute | undefined;
+  type?: HTMLInputTypeAttribute;
   value?: string;
-  onChange?: (update: ChangeEvent<HTMLInputElement>) => void;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
 };
 
 const TextInput = ({
@@ -16,22 +16,19 @@ const TextInput = ({
   onChange,
 }: TextInputProps) => {
   return (
-    <div className="relative flex flex-col">
+    <div className="relative flex w-full flex-col">
       <input
         type={type}
         aria-label={label}
         name={id}
         id={id}
         className="peer relative block h-14 w-full overflow-x-auto rounded-[20px] border border-gray-500 bg-neutral-10 px-4 py-2 leading-5 focus:border-2 focus:border-primary-600 focus:outline-none focus:ring-0"
-        placeholder=""
+        placeholder=" "
         value={value}
-        onChange={() => onChange}
+        onChange={onChange}
       />
 
-      <label
-        htmlFor={id}
-        className="absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform bg-neutral-10 px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:bg-neutral-10 peer-focus:px-1 peer-focus:text-primary-600"
-      >
+      <label className="absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform bg-neutral-10 px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:bg-neutral-10 peer-focus:px-1 peer-focus:text-primary-600">
         {label}
       </label>
       <div className="hidden px-4 pt-1 text-xs tracking-[0.4px]">

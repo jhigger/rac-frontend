6a7b3d2be51e352b267aae39a2d09528817ca62c
jhigger <njohnjhigger@gmail.com>
2023-11-30 18:02:55 +0800

    edit padding

--- src/components/Shop/TabContentLayout.tsx
+++ src/components/Shop/TabContentLayout.tsx
@@ -4,7 +4,7 @@ type TabContentLayoutProps = { children: ReactNode };
 
 const TabContentLayout = ({ children }: TabContentLayoutProps) => {
   return (
-    <div className="flex h-full min-h-[calc(100vh-210px)] w-full flex-col overflow-y-auto px-[20px] py-[23px] md:min-h-[calc(100vh-170px)]">
+    <div className="flex h-full min-h-[calc(100vh-210px)] w-full flex-col overflow-y-auto px-[20px] py-[23px] md:min-h-[calc(100vh-170px)] md:px-[40px]">
       {children}
     </div>
   );

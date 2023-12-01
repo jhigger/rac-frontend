166238a2e618d2af87e6653a0e6c1504c1358472
jhigger <njohnjhigger@gmail.com>
2023-12-02 02:41:45 +0800

    fix responsive width

--- src/components/Shop/TabContentLayout.tsx
+++ src/components/Shop/TabContentLayout.tsx
@@ -4,7 +4,7 @@ type TabContentLayoutProps = { children: ReactNode };
 
 const TabContentLayout = ({ children }: TabContentLayoutProps) => {
   return (
-    <div className="flex h-full min-h-[calc(100vh-210px)] w-full flex-col overflow-y-auto p-[20px] md:min-h-[calc(100vh-170px)] md:px-[40px]">
+    <div className="flex h-full min-h-[calc(100vh-210px)] w-full flex-col overflow-y-auto p-[20px] md:min-h-[calc(100vh-170px)] md:max-w-[calc(100vw-286px)] md:px-[40px]">
       {children}
     </div>
   );

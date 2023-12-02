3516653c289ce3761add78faa57f393874b03ec0
jhigger <njohnjhigger@gmail.com>
2023-12-03 05:38:08 +0800

    fix breadcrumbs mobile

--- src/components/TopAppBar/BreadCrumbs.tsx
+++ src/components/TopAppBar/BreadCrumbs.tsx
@@ -4,7 +4,7 @@ import { useShopContext } from "~/contexts/ShopContext";
 
 const BreadCrumbs = () => {
   const { activeNav } = useNavContext();
-  const { activeTab } = useShopContext();
+  const { activeTab, activeAction } = useShopContext();
 
   return (
     <div className="flex w-full items-center justify-center gap-[10px] md:gap-4">
@@ -13,18 +13,33 @@ const BreadCrumbs = () => {
         alt="home icon"
         className="h-[19px] w-[19px]"
       />
+
       <img src="/images/arrow_left_icon.svg" alt="arrow icon" />
       <span className="title-sm text-secondary-600">
-        {activeNav.toLowerCase()}
+        <div className="hidden [@media(min-width:500px)]:block">
+          {activeNav.toLowerCase()}
+        </div>
+        <div className="[@media(min-width:500px)]:hidden">
+          {activeAction ? "..." : activeNav.toLowerCase()}
+        </div>
       </span>
+
       <img src="/images/arrow_left_icon.svg" alt="arrow icon" />
-      <span className="title-sm text-secondary-600">{activeTab}</span>
-      {/* {activeAction !== "request new order" && activeAction && (
+      <span className="title-sm text-secondary-600">
+        <div className="hidden [@media(min-width:375px)]:block">
+          {activeTab}
+        </div>
+        <div className="[@media(min-width:375px)]:hidden">
+          {activeAction ? "..." : activeTab}
+        </div>
+      </span>
+
+      {activeAction && (
         <>
           <img src="/images/arrow_left_icon.svg" alt="arrow icon" />
           <span className="title-sm text-secondary-600">{activeAction}</span>
         </>
-      )} */}
+      )}
     </div>
   );
 };

48e9c8c5215ffca7b06b035d037d0c15e966e51f
jhigger <njohnjhigger@gmail.com>
2023-12-03 05:17:28 +0800

    fix top bar on top of nav drawer

--- src/components/Navigation/SlideSheet.tsx
+++ src/components/Navigation/SlideSheet.tsx
@@ -8,7 +8,7 @@ const SlideSheet = ({ id, name, src }: SlideSheetProps) => {
     <div id="sheet_b" className="group md:hidden">
       <div
         data-close="#sheet_b"
-        className="fixed -top-full z-40 bg-neutral-900 opacity-0 group-[&.show]:inset-0 group-[&.show]:opacity-60"
+        className="fixed -top-full z-50 bg-neutral-900 opacity-0 group-[&.show]:inset-0 group-[&.show]:opacity-60"
       ></div>
       <nav className="fixed bottom-0 left-0 top-0 z-50 flex h-full min-h-screen w-[266px] -translate-x-full flex-col overflow-y-auto bg-brand py-[40px] transition-transform duration-[400ms] group-[&.show]:translate-x-0">
         <Welcome

d3e12c88a4a4d83ddddc19487cb75fcf6fd73f6f
jhigger <njohnjhigger@gmail.com>
2023-12-03 01:07:23 +0800

    remove action

--- src/components/TopAppBar/BreadCrumbs.tsx
+++ src/components/TopAppBar/BreadCrumbs.tsx
@@ -4,10 +4,10 @@ import { useShopContext } from "~/contexts/ShopContext";
 
 const BreadCrumbs = () => {
   const { activeNav } = useNavContext();
-  const { activeTab, activeAction } = useShopContext();
+  const { activeTab } = useShopContext();
 
   return (
-    <div className="items-center justify-center flex w-full gap-[10px] md:gap-4">
+    <div className="flex w-full items-center justify-center gap-[10px] md:gap-4">
       <img
         src="/images/nav/home_icon.svg"
         alt="home icon"
@@ -19,12 +19,12 @@ const BreadCrumbs = () => {
       </span>
       <img src="/images/arrow_left_icon.svg" alt="arrow icon" />
       <span className="title-sm text-secondary-600">{activeTab}</span>
-      {activeAction !== "request new order" && (
+      {/* {activeAction !== "request new order" && activeAction && (
         <>
           <img src="/images/arrow_left_icon.svg" alt="arrow icon" />
           <span className="title-sm text-secondary-600">{activeAction}</span>
         </>
-      )}
+      )} */}
     </div>
   );
 };

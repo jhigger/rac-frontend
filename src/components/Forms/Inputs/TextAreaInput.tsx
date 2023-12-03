7c7fd115377bb2d2578a3ba430b37df2e66c4e18
jhigger <njohnjhigger@gmail.com>
2023-12-03 18:48:42 +0800

    add custom bg prop

--- src/components/Forms/Inputs/TextAreaInput.tsx
+++ src/components/Forms/Inputs/TextAreaInput.tsx
@@ -3,18 +3,25 @@ import { type ChangeEventHandler } from "react";
 type TextAreaInputProps = {
   id: string;
   label: string;
+  bg?: string;
   value?: string;
   onChange?: ChangeEventHandler<HTMLTextAreaElement>;
 };
 
-const TextAreaInput = ({ id, label, value, onChange }: TextAreaInputProps) => {
+const TextAreaInput = ({
+  id,
+  bg = "bg-neutral-10",
+  label,
+  value,
+  onChange,
+}: TextAreaInputProps) => {
   return (
     <div className="relative flex w-full flex-col">
       <textarea
         aria-label={label}
         name={id}
         id={id}
-        className="peer relative block h-[144px] w-full overflow-x-auto rounded-[20px] border border-gray-500 bg-neutral-10 px-4 py-2 leading-5 focus:border-2 focus:border-primary-600 focus:outline-none focus:ring-0"
+        className={`peer relative block h-[144px] w-full overflow-x-auto rounded-[20px] border border-gray-500 px-4 py-2 leading-5 focus:border-2 focus:border-primary-600 focus:outline-none focus:ring-0 ${bg}`}
         placeholder=" "
         value={value}
         onChange={onChange}
@@ -22,7 +29,9 @@ const TextAreaInput = ({ id, label, value, onChange }: TextAreaInputProps) => {
 
       <label
         htmlFor={id}
-        className="absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform bg-neutral-10 px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:bg-neutral-10 peer-focus:px-1 peer-focus:text-primary-600"
+        className={`absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:px-1 peer-focus:text-primary-600 ${
+          bg && `peer-focus:${bg}`
+        } ${bg}`}
       >
         {label}
       </label>

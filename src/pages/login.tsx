87a1a9ef848381690f944b125ef87ae33791f2aa
jhigger <njohnjhigger@gmail.com>
2023-12-04 07:49:10 +0800

    implement form handling

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,61 +1,50 @@
 import Link from "next/link";
-import { type ChangeEvent } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import FormHeader from "~/components/Forms/FormHeader";
-import Logo from "~/components/Logo";
-import NeedHelpFAB from "~/components/NeedHelpFAB";
 import PasswordInput from "~/components/Forms/Inputs/PasswordInput";
 import TextInput from "~/components/Forms/Inputs/TextInput";
+import Logo from "~/components/Logo";
+import NeedHelpFAB from "~/components/NeedHelpFAB";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const login = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-brand">
       <div className="container flex flex-col items-center justify-center px-14 py-16">
         <Logo />
 
-        <form className="mb-[30px] mt-[100px] flex w-full max-w-[658px] flex-col items-center justify-center gap-[54px] rounded-[20px] bg-white p-[50px]">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mb-[30px] mt-[100px] flex w-full max-w-[658px] flex-col items-center justify-center gap-[54px] rounded-[20px] bg-white p-[50px]"
+        >
           <FormHeader title="Login" />
           <div className="flex w-full max-w-[500px] flex-col gap-[30px]">
-            <EmailInput />
-            <PasswordInput id="password" label="Password" />
+            <TextInput
+              id="email"
+              label="Email"
+              type="email"
+              {...register("email")}
+            />
+            <PasswordInput
+              id="password"
+              label="Password"
+              {...register("password")}
+            />
           </div>
           <LoginButton />
         </form>
 
-        <div className="text-white">
-          <span className="label-lg">New to RAC Logistics? </span>
-          <Link
-            href="/register"
-            className="label-lg px-[12px] py-[10px] text-primary-200 hover:underline"
-          >
-            Sign up
-          </Link>
-        </div>
-        <a
-          href="#"
-          className="label-lg relative px-[12px] py-[10px] text-primary-200 hover:underline"
-        >
-          Forgot your password?
-        </a>
+        <Footer />
       </div>
       <NeedHelpFAB />
     </main>
-  );
-};
-
-type EmailInputProps = {
-  value?: string;
-  onChange?: (update: ChangeEvent<HTMLInputElement>) => void;
-};
-
-export const EmailInput = ({ value, onChange }: EmailInputProps) => {
-  return (
-    <TextInput
-      id="email"
-      label="Email"
-      type="email"
-      value={value}
-      onChange={onChange}
-    />
   );
 };
 
@@ -64,6 +53,28 @@ const LoginButton = () => {
     <button className="btn relative flex flex-row items-center justify-center gap-x-2 rounded-[6.25rem] bg-primary-600 px-6 py-2.5 text-sm font-medium tracking-[.00714em] text-white hover:shadow-md">
       Login to your account
     </button>
+  );
+};
+
+const Footer = () => {
+  return (
+    <>
+      <div className="text-white">
+        <span className="label-lg">New to RAC Logistics? </span>
+        <Link
+          href="/register"
+          className="label-lg px-[12px] py-[10px] text-primary-200 hover:underline"
+        >
+          Sign up
+        </Link>
+      </div>
+      <a
+        href="#"
+        className="label-lg relative px-[12px] py-[10px] text-primary-200 hover:underline"
+      >
+        Forgot your password?
+      </a>
+    </>
   );
 };
 

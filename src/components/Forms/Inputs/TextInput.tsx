2bed0e960f9b57117671e7cdd4159b9aa674e886
jhigger <njohnjhigger@gmail.com>
2023-12-03 16:11:20 +0800

    add custom bg prop

--- src/components/Forms/Inputs/TextInput.tsx
+++ src/components/Forms/Inputs/TextInput.tsx
@@ -7,6 +7,7 @@ import {
 type TextInputProps = {
   id: string;
   label: string;
+  bg?: string;
   disabled?: boolean;
   type?: HTMLInputTypeAttribute;
   value?: string;
@@ -16,6 +17,7 @@ type TextInputProps = {
 const TextInput = ({
   id,
   label,
+  bg = "bg-neutral-10",
   disabled,
   type = "text",
   value,
@@ -36,7 +38,7 @@ const TextInput = ({
         aria-label={label}
         name={id}
         id={id}
-        className="peer relative block h-14 w-full overflow-x-auto rounded-[20px] border border-gray-500 bg-neutral-10 px-4 py-2 leading-5 focus:border-2 focus:border-primary-600 focus:outline-none focus:ring-0"
+        className={`peer relative block h-14 w-full overflow-x-auto rounded-[20px] border border-gray-500 bg-neutral-10 px-4 py-2 leading-5 focus:border-2 focus:border-primary-600 focus:outline-none focus:ring-0 ${bg}`}
         placeholder=" "
         disabled={disabled}
         value={value}
@@ -45,7 +47,9 @@ const TextInput = ({
 
       <label
         onClick={handleFocusInput}
-        className="absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform bg-neutral-10 px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:bg-neutral-10 peer-focus:px-1 peer-focus:text-primary-600"
+        className={`absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 ${
+          bg && `peer-focus:${bg}`
+        } peer-focus:px-1 peer-focus:text-primary-600 ${bg}`}
       >
         {label}
       </label>

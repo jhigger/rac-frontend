81bf6d7aa0307b114bf378f0dfb79ae175f6d329
jhigger <njohnjhigger@gmail.com>
2023-12-03 18:29:34 +0800

    edit

--- src/components/Forms/Inputs/TextInput.tsx
+++ src/components/Forms/Inputs/TextInput.tsx
@@ -38,7 +38,7 @@ const TextInput = ({
         aria-label={label}
         name={id}
         id={id}
-        className={`peer relative block h-14 w-full overflow-x-auto rounded-[20px] border border-gray-500 bg-neutral-10 px-4 py-2 leading-5 focus:border-2 focus:border-primary-600 focus:outline-none focus:ring-0 ${bg}`}
+        className={`peer relative block h-14 w-full overflow-x-auto rounded-[20px] border border-gray-500 px-4 py-2 leading-5 focus:border-2 focus:border-primary-600 focus:outline-none focus:ring-0 ${bg}`}
         placeholder=" "
         disabled={disabled}
         value={value}
@@ -47,9 +47,9 @@ const TextInput = ({
 
       <label
         onClick={handleFocusInput}
-        className={`absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 ${
+        className={`absolute left-4 top-4 z-10 origin-[0] -translate-y-7 scale-75 transform px-1 tracking-[.03125em] text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-invalid:text-error-600 peer-focus:left-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:px-1 peer-focus:text-primary-600 ${
           bg && `peer-focus:${bg}`
-        } peer-focus:px-1 peer-focus:text-primary-600 ${bg}`}
+        } ${bg}`}
       >
         {label}
       </label>

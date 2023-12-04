a76c534490029440746931fd352f92c88bce284e
jhigger <njohnjhigger@gmail.com>
2023-12-04 13:24:37 +0800

    add loading spinner

--- src/components/Forms/Register/AddressForm.tsx
+++ src/components/Forms/Register/AddressForm.tsx
@@ -24,6 +24,23 @@ const AddressForm = ({ register, getValues }: AddressFormProps) => {
     handleStates(getValues("country"));
   }, [getValues("country")]);
 
+  if (!states)
+    return (
+      <div className="relative flex flex-col items-center justify-center">
+        <svg className="circular-loader relative h-[100px] w-[100px]">
+          <circle
+            className="path stroke-primary-600"
+            cx="50"
+            cy="50"
+            r="20"
+            fill="none"
+            strokeWidth="5"
+            strokeMiterlimit="10"
+          ></circle>
+        </svg>
+      </div>
+    );
+
   return (
     <>
       <FormHeader

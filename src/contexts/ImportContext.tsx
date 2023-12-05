9f10c3b84a0131499bd9453d45667661c7aac7c9
jhigger <njohnjhigger@gmail.com>
2023-12-06 06:45:41 +0800

    add import context

---
+++ src/contexts/ImportContext.tsx
@@ -0,0 +1,115 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+import { orders, requests } from "~/fake data";
+
+export type ImportContextType = {
+  orderItems: OrderItemType[] | null;
+  payNowAction: { action: () => void } | null;
+  properties: PropertyType[] | null;
+  requestItems: RequestItemType[] | null;
+  handlePayNowAction: (action: ImportContextType["payNowAction"]) => void;
+  handleOrders: () => void;
+  handleProperties: (p: PropertyType[] | null) => void;
+  handleRequests: () => void;
+};
+
+export const ImportContext = createContext<ImportContextType>(
+  {} as ImportContextType,
+);
+
+export const useImportContext = () => useContext(ImportContext);
+
+const ORDER_STATUS = ["responded", "processed", "not responded"] as const;
+const SHIPPING_STATUS = [
+  "ready for shipping",
+  "not started",
+  "processing",
+  "cancelled",
+  "in transit",
+  "arrived destination",
+  "cleared",
+  "delivered",
+] as const;
+const SHOP_FOR_ME_STATUS = [
+  "purchase not started",
+  "purchase in progress",
+  "purchase completed",
+] as const;
+
+export type OrderItemType = {
+  images: string[];
+  orderId: string;
+  orderStatus: (typeof ORDER_STATUS)[number];
+  orderDate: string;
+  trackingId: string;
+  shippingStatus: (typeof SHIPPING_STATUS)[number];
+  shopForMeStatus: (typeof SHOP_FOR_ME_STATUS)[number];
+  shopForMeCost: string;
+  shippingCost: string;
+};
+
+const REQUEST_STATUS = ["responded", "not responded"] as const;
+
+export type RequestItemType = {
+  images: string[];
+  requestId: string;
+  requestStatus: (typeof REQUEST_STATUS)[number];
+  requestDate: string;
+};
+
+export type PropertyType = { label: string; value: string | undefined };
+
+const ImportContextProvider = ({ children }: { children: ReactNode }) => {
+  const [orderItems, setOrderItems] = useState<OrderItemType[] | null>(null);
+  const [payNowAction, setPayNowAction] =
+    useState<ImportContextType["payNowAction"]>(null);
+  const [requestItems, setRequestItems] = useState<RequestItemType[] | null>(
+    null,
+  );
+  const [properties, setProperties] = useState<PropertyType[] | null>(null);
+
+  const handleOrders = () => {
+    setOrderItems(orders);
+  };
+
+  const handlePayNowAction = (action: ImportContextType["payNowAction"]) => {
+    setPayNowAction(action);
+  };
+
+  const handleProperties = (p: PropertyType[] | null) => {
+    setProperties(p);
+  };
+
+  const handleRequests = () => {
+    handleOrders();
+    setRequestItems(requests);
+  };
+
+  // testing purposes
+  useEffect(() => {
+    handleRequests();
+    handleOrders();
+  }, []);
+
+  const value: ImportContextType = {
+    orderItems,
+    payNowAction,
+    properties,
+    requestItems,
+    handleOrders,
+    handlePayNowAction,
+    handleProperties,
+    handleRequests,
+  };
+
+  return (
+    <ImportContext.Provider value={value}>{children}</ImportContext.Provider>
+  );
+};
+
+export default ImportContextProvider;
--- src/pages/import.tsx
+++ src/pages/import.tsx
@@ -3,6 +3,7 @@ import SlideSheet from "~/components/Navigation/SlideSheet";
 import Welcome from "~/components/Navigation/Welcome";
 import TopAppBar from "~/components/TopAppBar";
 import { useAuthContext } from "~/contexts/AuthContext";
+import ImportContextProvider from "~/contexts/ImportContext";
 import TabContextProvider from "~/contexts/TabContext";
 
 const importPage = () => {
@@ -12,29 +13,31 @@ const importPage = () => {
 
   return (
     <TabContextProvider>
-      <div className="relative flex min-h-screen bg-neutral-50">
-        <nav className="fixed hidden h-full min-h-screen w-[266px] flex-col overflow-y-auto bg-brand py-[40px] md:flex">
-          <Welcome
+      <ImportContextProvider>
+        <div className="relative flex min-h-screen bg-neutral-50">
+          <nav className="fixed hidden h-full min-h-screen w-[266px] flex-col overflow-y-auto bg-brand py-[40px] md:flex">
+            <Welcome
+              {...{
+                id: "RAC45678",
+                name: "Rex",
+                src: "https://placehold.co/400x400/cac4d0/1d192b?text=R&font=roboto",
+              }}
+            />
+            <TopNav />
+            <BottomNav />
+          </nav>
+          <SlideSheet
             {...{
               id: "RAC45678",
               name: "Rex",
               src: "https://placehold.co/400x400/cac4d0/1d192b?text=R&font=roboto",
             }}
           />
-          <TopNav />
-          <BottomNav />
-        </nav>
-        <SlideSheet
-          {...{
-            id: "RAC45678",
-            name: "Rex",
-            src: "https://placehold.co/400x400/cac4d0/1d192b?text=R&font=roboto",
-          }}
-        />
-        <main className="w-full md:ml-[266px]">
-          <TopAppBar />
-        </main>
-      </div>
+          <main className="w-full md:ml-[266px]">
+            <TopAppBar />
+          </main>
+        </div>
+      </ImportContextProvider>
     </TabContextProvider>
   );
 };

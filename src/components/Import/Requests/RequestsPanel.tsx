d908dd99f4376951001c3323d46c82215c299e10
jhigger <njohnjhigger@gmail.com>
2023-12-06 09:57:03 +0800

    add table

--- src/components/Import/Requests/RequestsPanel.tsx
+++ src/components/Import/Requests/RequestsPanel.tsx
@@ -1,12 +1,23 @@
-import React from "react";
 import Balancer from "react-wrap-balancer";
+import NeedHelpFAB from "~/components/NeedHelpFAB";
+import {
+  TableFooter,
+  type TableHeadType,
+} from "~/components/Shop/Orders/OrdersPanel";
 import RequestOrderButton from "~/components/Shop/RequestOrderButton";
-import TabContentLayout from "~/components/Shop/TabContentLayout";
+import {
+  RequestTableBody,
+  RequestTableHead,
+} from "~/components/Shop/Requests/RequestsPanel";
+import SearchBar from "~/components/Shop/SearchBar";
+import TabContentLayout from "~/components/Layouts/TabContentLayout";
+import { useImportContext } from "~/contexts/ImportContext";
 import { useTabContext } from "~/contexts/TabContext";
 import RequestOrder from "./RequestOrder";
 
 const ImportRequestsPanel = () => {
   const { activeAction } = useTabContext();
+  const { requestItems } = useImportContext();
 
   if (activeAction === "request new order") {
     return (
@@ -15,6 +26,25 @@ const ImportRequestsPanel = () => {
       </TabContentLayout>
     );
   }
+
+  if (activeAction === "request details") {
+    return <TabContentLayout>{/* <RequestDetails /> */}</TabContentLayout>;
+  }
+
+  if (activeAction === "proceed to checkout") {
+    return <TabContentLayout>{/* <RequestCheckout /> */}</TabContentLayout>;
+  }
+
+  if (requestItems) {
+    return (
+      <TabContentLayout>
+        <SearchBar />
+        <RequestsTable />
+        <NeedHelpFAB />
+      </TabContentLayout>
+    );
+  }
+
   return (
     <TabContentLayout>
       <div className="flex w-full flex-grow flex-col items-center justify-center gap-[30px]">
@@ -27,6 +57,33 @@ const ImportRequestsPanel = () => {
         <RequestOrderButton />
       </div>
     </TabContentLayout>
+  );
+};
+
+const tableHeads: TableHeadType[] = [
+  { title: "Package(s) Image", sortIcon: false },
+  { title: "Request ID", sortIcon: true },
+  { title: "Request Status", sortIcon: false },
+  { title: "Request Date", sortIcon: true },
+];
+
+const RequestsTable = () => {
+  const { requestItems } = useImportContext();
+
+  if (!requestItems) return;
+
+  return (
+    <div className="flex w-full flex-col gap-[10px] rounded-[20px] bg-white p-[10px] md:p-[20px]">
+      <div className="flex flex-col gap-[20px]">
+        <div className="overflow-x-scroll ">
+          <table className="relative w-full min-w-max table-auto text-left">
+            <RequestTableHead th={tableHeads} />
+            <RequestTableBody requestItems={requestItems} />
+          </table>
+        </div>
+      </div>
+      <TableFooter />
+    </div>
   );
 };
 

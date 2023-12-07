cd92e5233e8c8faefdd4bada50952a0622db0f0c
jhigger <njohnjhigger@gmail.com>
2023-12-07 11:42:23 +0800

    add auth guard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import Logo from "~/components/Logo";
+import { useAuthContext } from "~/contexts/AuthContext";
 
 export default function Home() {
+  const { user } = useAuthContext();
+
+  if (!user) return null;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-brand">
       <div className="container flex flex-col items-center justify-center px-14 py-16">

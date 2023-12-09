c9ea0ffc10fd8459b98c8857931d8b7c792ba2b8
jhigger <njohnjhigger@gmail.com>
2023-12-09 14:19:10 +0800

    fix submitting checkbox form

--- src/components/Forms/AccordionButton.tsx
+++ src/components/Forms/AccordionButton.tsx
@@ -5,6 +5,7 @@ type AccordionButtonProps = { open: boolean; toggle: () => void };
 const AccordionButton = ({ open, toggle }: AccordionButtonProps) => {
   return (
     <button
+      type="button"
       aria-label={open ? "collapse" : "expand"}
       onClick={toggle}
       className="flex h-[24px] w-[24px] items-center justify-center rounded-[6.25rem] hover:bg-surface-300 focus:bg-surface-400"

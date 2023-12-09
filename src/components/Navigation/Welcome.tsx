343783436a534724ef877dc0afb658bc524e011e
jhigger <njohnjhigger@gmail.com>
2023-12-09 21:53:42 +0800

    edit

--- src/components/Navigation/Welcome.tsx
+++ src/components/Navigation/Welcome.tsx
@@ -5,7 +5,7 @@ const Welcome = () => {
   const { user } = useAuthContext();
 
   return (
-    <div className="mb-[28px] h-[80px] w-[253px] rounded-r-[20px] bg-neutral-100 bg-opacity-[8%]">
+    <div className="mb-[28px] w-[253px] rounded-r-[20px] bg-neutral-100 bg-opacity-[8%]">
       <div className="flex items-center gap-[10px] px-[10px] py-[18px]">
         <img
           src={"https://placehold.co/400x400/cac4d0/1d192b?text=R&font=roboto"}
@@ -13,12 +13,10 @@ const Welcome = () => {
           className="h-[40px] w-[40px] rounded-full bg-red-500"
         />
         <div className="flex flex-col gap-0 text-gray-100">
-          <span className="body-md">Welcome back</span>
+          <span className="body-md">Welcome back,</span>
+          <span>{user?.firstName}</span>
           <div className="body-lg flex gap-[10px]">
-            {/* <span>{"Rex"}</span> */}
-            <span className="font-bold text-white">
-              ID: RAC{user?._id.slice(0, 5)}
-            </span>
+            <span className="font-bold text-white">ID: RAC{"45678"}</span>
           </div>
         </div>
       </div>

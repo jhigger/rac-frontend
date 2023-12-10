1af152bf31aa67d8653f94836855ddd640b598d5
jhigger <njohnjhigger@gmail.com>
2023-12-10 15:29:24 +0800

    change new order button icon for different pages

--- src/components/Shop/RequestOrderButton.tsx
+++ src/components/Shop/RequestOrderButton.tsx
@@ -1,7 +1,8 @@
-import { Shop } from "iconsax-react";
+import { navItems, useNavContext } from "~/contexts/NavigationContext";
 import { useTabContext } from "~/contexts/TabContext";
 
 const RequestOrderButton = () => {
+  const { activeNav } = useNavContext();
   const { handleActiveAction, handleTabChange } = useTabContext();
 
   const onClick = () => {
@@ -15,7 +16,9 @@ const RequestOrderButton = () => {
       aria-label="Request new order"
       className="btn relative flex flex-row items-center justify-center gap-x-2 rounded-[6.25rem] bg-primary-600 px-4 py-2.5 text-sm font-medium tracking-[.00714em] text-white md:px-6"
     >
-      <Shop variant="Bold" />
+      <span className="[&>*]:text-white">
+        {navItems.find((navItem) => activeNav === navItem.title)?.src}
+      </span>
       <span>Request new order</span>
     </button>
   );

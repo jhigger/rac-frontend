a20a4829843e5776c1f27d9664dcdc465e4812ac
jhigger <njohnjhigger@gmail.com>
2024-01-26 20:12:07 +0800

    refactor

--- src/components/Buttons/MoreButton.tsx
+++ src/components/Buttons/MoreButton.tsx
@@ -1,24 +1,27 @@
+import { Menu } from "@headlessui/react";
 import { More } from "iconsax-react";
 
 type MoreButtonProps = { onClick: () => void };
 
 export const MoreButton = ({ onClick }: MoreButtonProps) => {
   return (
-    <div className="group relative inline-block">
-      <button className="peer flex h-12 w-12 items-center justify-center rounded-[6.25rem] hover:bg-surface-300 focus:bg-surface-400">
-        <More className="text-error-600" />
-      </button>
+    <Menu as="div" className="relative inline-block">
+      <div>
+        <Menu.Button className="flex h-12 w-12 items-center justify-center rounded-[6.25rem] hover:bg-surface-300 focus:bg-surface-400">
+          <More className="text-error-600" />
+        </Menu.Button>
+      </div>
 
-      <ul className="absolute right-0 top-10 z-50 hidden min-w-[200px] flex-col items-center justify-center rounded-[10px] bg-surface-200 shadow-md group-focus-within:inline-flex peer-focus:inline-flex">
-        <li className="w-full">
+      <Menu.Items className="absolute right-0 top-10 z-50 min-w-[200px] flex-col items-center justify-center rounded-[10px] bg-surface-200 shadow-md">
+        <Menu.Item>
           <button
-            className="relative w-full rounded-[10px] px-4 py-2 hover:bg-secondary-100 hover:bg-opacity-30"
+            className="w-full rounded-[10px] px-4 py-2 hover:bg-secondary-100 hover:bg-opacity-30"
             onClick={onClick}
           >
             View Details
           </button>
-        </li>
-      </ul>
-    </div>
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
   );
 };

6dab2ca25096c0ed7877de5e0fda90778bfa6638
jhigger <njohnjhigger@gmail.com>
2024-01-28 23:38:46 +0800

    add grid layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,18 @@ const Home = () => {
       <TrackingContextProvider>
         <PageLayout>
           <TopAppBar hasTabs={false} hasBreadcrumbs={false} />
-          <NoTabsContentLayout>Home</NoTabsContentLayout>
+          <NoTabsContentLayout>
+            <div className="grid h-full w-full flex-grow grid-cols-1 gap-[20px] overflow-auto md:grid-cols-3 md:grid-rows-4">
+              <div className="col-span-full rounded-[20px] border border-gray-200 bg-white p-[20px] md:col-span-1 md:row-span-1"></div>
+              <div className="col-span-full rounded-[20px] border border-gray-200 bg-white p-[20px] md:col-span-1 md:row-span-2"></div>
+              <div className="col-span-full rounded-[20px] border border-gray-200 bg-white p-[20px] md:col-span-1 md:row-span-4"></div>
+              <div className="col-span-full rounded-[20px] border border-gray-200 bg-white p-[20px] md:col-span-1 md:row-span-1"></div>
+              <div className="col-span-full flex flex-col md:col-span-2 md:row-span-2">
+                <div className="rounded-[20px] border border-gray-200 bg-white p-[20px]"></div>
+                <div className="h-full rounded-[20px] border border-gray-200 bg-white p-[20px]"></div>
+              </div>
+            </div>
+          </NoTabsContentLayout>
         </PageLayout>
       </TrackingContextProvider>
     </TabContextProvider>
